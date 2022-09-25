bf7bb39619dac77984aa009b9acaf5b0549a21eb
Alejandro Soto <alejandro@34project.org>
2022-09-25 15:05:08 -0600

    Add fetch jump target

--- rtl/core/fetch/fetch.sv
+++ rtl/core/fetch/fetch.sv
@@ -3,19 +3,20 @@
 module core_fetch
 #(parameter PREFETCH_ORDER=2)
 (
-	input  logic       clk,
-	                   stall,
-	                   flush,
-	                   fetched,
-	input  logic[31:0] fetch_data,
+	input  logic clk,
+	             stall,
+	             flush,
+	             fetched,
+	input  word  fetch_data,
+	input  ptr   target,
 
-	output logic       fetch,
-	output logic[31:0] insn,
-	output logic[29:0] insn_pc,
-	                   addr
+	output logic fetch,
+	output word  insn,
+	output ptr   insn_pc,
+	             addr
 );
 
-	logic[29:0] next_pc;
+	ptr next_pc;
 
 	core_prefetch #(.ORDER(PREFETCH_ORDER)) prefetch
 	(
@@ -28,8 +29,6 @@ module core_fetch
 		else if(fetched)
 			addr <= addr + 1;
 
-	initial begin
-		addr = 0;
-	end
+	initial addr = 0;
 
 endmodule
