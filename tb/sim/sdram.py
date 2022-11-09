bf82f69cbf7fa0900263236057174c421a28045c
Alejandro Soto <alejandro@34project.org>
2022-11-09 09:54:34 -0600

    Improve sdram sim test

--- tb/sim/sdram.py
+++ tb/sim/sdram.py
@@ -1,8 +1,8 @@
-START = 0x0000_1000
+START = 0x0000_1004
 END   = 0x0000_2000
 
 cycles = 30000
 mem_dumps = [range(START, END)]
 
 def final():
-    assert_mem(START, list(range(0, (END - START) >> 2)))
+    assert_mem(START, list(range(42, 42 + (END - START) >> 2)))
