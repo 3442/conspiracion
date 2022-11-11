07d9046a69600bee983c464ea8ee16fe2d959729
Alejandro Soto <alejandro@34project.org>
2022-11-10 18:21:53 -0600

    Simplify stall conditions to reflect uarch changes

--- rtl/core/control/stall.sv
+++ rtl/core/control/stall.sv
@@ -19,20 +19,22 @@ module core_control_stall
 	                   next_bubble
 );
 
-	logic pc_writeback_hazard, flags_hazard, data_hazard, rn_hazard,
-	      snd_hazard, flags_dependency, updating_flags;
+	logic pc_rd_hazard, pc_wr_hazard, rn_pc_hazard, snd_pc_hazard,
+		  flags_hazard, flags_dependency, updating_flags;
 
 	assign stall = next_cycle != ISSUE || next_bubble;
-	assign next_bubble = pc_writeback_hazard || flags_hazard || data_hazard;
+	assign next_bubble = pc_rd_hazard || pc_wr_hazard || flags_hazard;
 
-	assign pc_writeback_hazard = final_writeback && final_rd == `R15;
+	//FIXME: pc_rd_hazard no debería definirse sin final_writeback?
+	assign pc_rd_hazard = final_writeback && (rn_pc_hazard || snd_pc_hazard);
+	assign pc_wr_hazard = final_writeback && final_rd == `R15;
+	assign rn_pc_hazard = dec.data.uses_rn && dec.data.rn == `R15;
+	assign snd_pc_hazard = !dec.snd.is_imm && dec.snd.r == `R15;
+
 	assign flags_hazard = flags_dependency && updating_flags;
-	assign data_hazard = final_writeback && (rn_hazard || snd_hazard);
-	assign rn_hazard = dec.data.uses_rn && (final_rd == dec.data.rn || dec.data.rn == `R15);
-	assign snd_hazard = !dec.snd.is_imm && (dec.snd.r == final_rd || dec.snd.r == `R15);
 
-	assign flags_dependency = dec.psr.update_flags || dec.ctrl.conditional;
 	assign updating_flags = final_update_flags || update_flags;
+	assign flags_dependency = dec.psr.update_flags || dec.ctrl.conditional;
 
 	always_ff @(posedge clk or negedge rst_n)
 		bubble <= !rst_n ? 0 : next_cycle == ISSUE && next_bubble;
