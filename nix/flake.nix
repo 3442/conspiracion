d564e3538a3654facc94bb4cd1ee021830dfcf52
Alejandro Soto <alejandro@34project.org>
2022-12-13 20:56:50 -0600

    Update Nix dev shells

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,41 +3,59 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
   in {
-    devShells."${system}".default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        ncurses
-        openssl
-        SDL2
-      ];
+    devShells."${system}" = with pkgs; {
+      default = mkShell {
+        buildInputs = [
+          ncurses
+          openssl
+          SDL2
+        ];
 
-      nativeBuildInputs = with pkgs; [
-        bc
-        binutils
-        bison
-        flex
-        gcc
-        gcc-arm-embedded
-        gdb
-        gnumake
-        gtkwave
-        pkg-config
-        (python39.withPackages (py: [ py.numpy py.pillow py.matplotlib ]))
-        (quartus-prime-lite.override { supportedDevices = [ "Cyclone V" ]; })
-        verilator
-      ];
+        nativeBuildInputs = [
+          binutils
+          gcc
+          gcc-arm-embedded
+          gdb
+          gnumake
+          gtkwave
+          pkg-config
+          (python39.withPackages (py: [ py.numpy py.pillow py.matplotlib ]))
+          (quartus-prime-lite.override { supportedDevices = [ "Cyclone V" ]; })
+          verilator
+        ];
 
-      shellHook = ''
-        # U-Boot
-        export CROSS_COMPILE=arm-none-eabi-
+        shellHook = ''
+          export MAKEFLAGS="AR=gcc-ar"
+          export CXXFLAGS="-O3 -flto $(pkg-config --cflags sdl2 ncursesw)"
+          export LDFLAGS="-O3 -flto $(pkg-config --libs sdl2 ncursesw)"
 
-        export MAKEFLAGS="AR=gcc-ar"
-        export CXXFLAGS="-O3 -flto $(pkg-config --cflags sdl2 ncursesw)"
-        export LDFLAGS="-O3 -flto $(pkg-config --libs sdl2 ncursesw)"
+          # <https://discourse.nixos.org/t/fonts-in-nix-installed-packages-on-a-non-nixos-system/5871/7>
+          export LOCALE_ARCHIVE="${glibcLocales}/lib/locale/locale-archive"
+          export FONTCONFIG_FILE="${fontconfig.out}/etc/fonts/fonts.conf"
+        '';
+      };
 
-        # <https://discourse.nixos.org/t/fonts-in-nix-installed-packages-on-a-non-nixos-system/5871/7>
-        export LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive"
-        export FONTCONFIG_FILE="${pkgs.fontconfig.out}/etc/fonts/fonts.conf"
-      '';
+      kbuild = mkShell {
+        buildInputs = [
+          ncurses
+        ];
+
+        nativeBuildInputs = [
+          bc
+          bison
+          flex
+          gcc
+          gcc-arm-embedded
+          gnumake
+          openssl # Splash de u-boot
+          ubootTools
+        ];
+
+        shellHook = ''
+          export CROSS_COMPILE=arm-none-eabi-
+          export MAKEFLAGS="ARCH=arm O=build/taller LOADADDR=0x8000"
+        '';
+      };
     };
   };
 }
