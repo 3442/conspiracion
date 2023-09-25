146168ca340e435eae6e30e99c06d752719089e3
Alejandro Soto <alejandro@34project.org>
2023-09-24 20:47:46 -0600

    nix/flake: enable formatter

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,152 +1,158 @@
 {
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
 
-    crossSystem = "arm-linux";
-    cross = import nixpkgs {
-      inherit system;
+      crossSystem = "arm-linux";
+      cross = import nixpkgs {
+        inherit system;
 
-      config.allowUnsupportedSystem = true;
+        config.allowUnsupportedSystem = true;
 
-      crossSystem = {
-        config = "arm-unknown-linux-gnueabi";
+        crossSystem = {
+          config = "arm-unknown-linux-gnueabi";
 
-        gcc = {
-          # > Switch "--with-arch" may not be used with switch "--with-cpu"
-          # > make[1]: *** [Makefile:4315: configure-gcc] Error 1
-          #arch = "armv4";
+          gcc = {
+            # > Switch "--with-arch" may not be used with switch "--with-cpu"
+            # > make[1]: *** [Makefile:4315: configure-gcc] Error 1
+            #arch = "armv4";
 
-          # Ver SA110 en arch/arm/mm/Kconfig, es parecido 
-          cpu = "arm810";
-        };
+            # Ver SA110 en arch/arm/mm/Kconfig, es parecido 
+            cpu = "arm810";
+          };
 
-        linux-kernel = {
-          name = "taller";
-          target = "uImage";
-          makeFlags = [ "LOADADDR=0x8000" ];
-          autoModules = false;
-          baseConfig = "taller_defconfig";
+          linux-kernel = {
+            name = "taller";
+            target = "uImage";
+            makeFlags = [ "LOADADDR=0x8000" ];
+            autoModules = false;
+            baseConfig = "taller_defconfig";
+          };
         };
       };
-    };
-  in {
-    # Tomado de pkgs/build-support/vm/default.nix
-    packages."${crossSystem}".proof-of-concept = cross.makeInitrd {
-      contents = [
-        {
-          symlink = "/init";
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
 
-          object = with cross; let
-            initrdUtils = runCommand "initrd-utils"
-              { nativeBuildInputs = [ buildPackages.nukeReferences ];
-                allowedReferences = [ "out" ]; # prevent accidents like glibc being included in the initrd
-              }
-              ''
-                mkdir -p $out/bin
-                mkdir -p $out/lib
+      # Tomado de pkgs/build-support/vm/default.nix
+      packages."${crossSystem}".proof-of-concept = cross.makeInitrd {
+        contents = [
+          {
+            symlink = "/init";
 
-                # Copy what we need from Glibc.
-                cp -p ${cross.glibc.out}/lib/ld-linux*.so.? $out/lib
-                cp -p ${cross.glibc.out}/lib/libc.so.* $out/lib
-                cp -p ${cross.glibc.out}/lib/libm.so.* $out/lib
-                cp -p ${cross.glibc.out}/lib/libresolv.so.* $out/lib
+            object = with cross; let
+              initrdUtils = runCommand "initrd-utils"
+                {
+                  nativeBuildInputs = [ buildPackages.nukeReferences ];
+                  allowedReferences = [ "out" ]; # prevent accidents like glibc being included in the initrd
+                }
+                ''
+                  mkdir -p $out/bin
+                  mkdir -p $out/lib
 
-                # Copy BusyBox.
-                cp -pd ${cross.busybox}/bin/* $out/bin
+                  # Copy what we need from Glibc.
+                  cp -p ${cross.glibc.out}/lib/ld-linux*.so.? $out/lib
+                  cp -p ${cross.glibc.out}/lib/libc.so.* $out/lib
+                  cp -p ${cross.glibc.out}/lib/libm.so.* $out/lib
+                  cp -p ${cross.glibc.out}/lib/libresolv.so.* $out/lib
 
-                # Run patchelf to make the programs refer to the copied libraries.
-                for i in $out/bin/* $out/lib/*; do if ! test -L $i; then nuke-refs $i; fi; done
+                  # Copy BusyBox.
+                  cp -pd ${cross.busybox}/bin/* $out/bin
 
-                for i in $out/bin/*; do
-                    if [ -f "$i" -a ! -L "$i" ]; then
-                        echo "patching $i..."
-                        patchelf --set-interpreter $out/lib/ld-linux*.so.? --set-rpath $out/lib $i || true
-                    fi
-                done
-              '';
+                  # Run patchelf to make the programs refer to the copied libraries.
+                  for i in $out/bin/* $out/lib/*; do if ! test -L $i; then nuke-refs $i; fi; done
+
+                  for i in $out/bin/*; do
+                      if [ -f "$i" -a ! -L "$i" ]; then
+                          echo "patching $i..."
+                          patchelf --set-interpreter $out/lib/ld-linux*.so.? --set-rpath $out/lib $i || true
+                      fi
+                  done
+                '';
 
 
-            path = lib.makeSearchPath "bin" [
-              #bashInteractive
-              #coreutils-full
-              #gnugrep
-              #neofetch
-              #util-linux
-            ];
-          in writeScript "init" ''
-            #!${initrdUtils}/bin/ash
+              path = lib.makeSearchPath "bin" [
+                #bashInteractive
+                #coreutils-full
+                #gnugrep
+                #neofetch
+                #util-linux
+              ];
+            in
+            writeScript "init" ''
+              #!${initrdUtils}/bin/ash
 
-            export PATH=${initrdUtils}/bin
+              export PATH=${initrdUtils}/bin
 
-            mkdir -p /dev /etc /proc /sys
-            echo -n > /etc/fstab
+              mkdir -p /dev /etc /proc /sys
+              echo -n > /etc/fstab
 
-            mount -t devtmpfs devtmpfs /dev
-            mount -t proc none /proc
-            mount -t sysfs none /sys
+              mount -t devtmpfs devtmpfs /dev
+              mount -t proc none /proc
+              mount -t sysfs none /sys
 
-            exec ash
+              exec ash
+            '';
+          }
+        ];
+      };
+
+      devShells."${system}" = with pkgs; {
+        default = mkShell {
+          buildInputs = [
+            ncurses
+            openssl
+            SDL2
+          ];
+
+          nativeBuildInputs = [
+            binutils
+            gcc
+            cross.stdenv.cc.cc
+            cross.stdenv.cc.bintools
+            gdb
+            gnumake
+            gtkwave
+            pkg-config
+            (python3.withPackages (py: [ py.numpy py.pillow py.matplotlib ]))
+            (quartus-prime-lite.override { supportedDevices = [ "Cyclone V" ]; })
+            verilator
+          ];
+
+          shellHook = ''
+            export MAKEFLAGS="AR=gcc-ar"
+            export CXXFLAGS="-O3 -flto $(pkg-config --cflags sdl2 ncursesw)"
+            export LDFLAGS="-O3 -flto $(pkg-config --libs sdl2 ncursesw)"
+
+            # <https://discourse.nixos.org/t/fonts-in-nix-installed-packages-on-a-non-nixos-system/5871/7>
+            export LOCALE_ARCHIVE="${glibcLocales}/lib/locale/locale-archive"
+            export FONTCONFIG_FILE="${fontconfig.out}/etc/fonts/fonts.conf"
           '';
-        }
-      ];
-    };
+        };
 
-    devShells."${system}" = with pkgs; {
-      default = mkShell {
-        buildInputs = [
-          ncurses
-          openssl
-          SDL2
-        ];
+        kbuild = mkShell {
+          buildInputs = [
+            ncurses
+          ];
 
-        nativeBuildInputs = [
-          binutils
-          gcc
-          cross.stdenv.cc.cc
-          cross.stdenv.cc.bintools
-          gdb
-          gnumake
-          gtkwave
-          pkg-config
-          (python3.withPackages (py: [ py.numpy py.pillow py.matplotlib ]))
-          (quartus-prime-lite.override { supportedDevices = [ "Cyclone V" ]; })
-          verilator
-        ];
+          nativeBuildInputs = [
+            bc
+            bison
+            flex
+            gcc
+            cross.stdenv.cc.cc
+            cross.stdenv.cc.bintools
+            gnumake
+            openssl # Splash de u-boot
+            ubootTools
+          ];
 
-        shellHook = ''
-          export MAKEFLAGS="AR=gcc-ar"
-          export CXXFLAGS="-O3 -flto $(pkg-config --cflags sdl2 ncursesw)"
-          export LDFLAGS="-O3 -flto $(pkg-config --libs sdl2 ncursesw)"
-
-          # <https://discourse.nixos.org/t/fonts-in-nix-installed-packages-on-a-non-nixos-system/5871/7>
-          export LOCALE_ARCHIVE="${glibcLocales}/lib/locale/locale-archive"
-          export FONTCONFIG_FILE="${fontconfig.out}/etc/fonts/fonts.conf"
-        '';
-      };
-
-      kbuild = mkShell {
-        buildInputs = [
-          ncurses
-        ];
-
-        nativeBuildInputs = [
-          bc
-          bison
-          flex
-          gcc
-          cross.stdenv.cc.cc
-          cross.stdenv.cc.bintools
-          gnumake
-          openssl # Splash de u-boot
-          ubootTools
-        ];
-
-        shellHook = ''
-          export CROSS_COMPILE=arm-none-eabi-
-          export MAKEFLAGS="ARCH=arm O=build/taller LOADADDR=0x8000"
-        '';
+          shellHook = ''
+            export CROSS_COMPILE=arm-none-eabi-
+            export MAKEFLAGS="ARCH=arm O=build/taller LOADADDR=0x8000"
+          '';
+        };
       };
     };
-  };
 }
