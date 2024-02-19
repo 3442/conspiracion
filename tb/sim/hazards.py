a9ba2e1d3e0bee4f7534b29f266d122567d6dd42
Alejandro Soto <alejandro@34project.org>
2024-02-19 08:36:55 -0600

    tb/sim: update constant in hazards test

--- tb/sim/hazards.py
+++ tb/sim/hazards.py
@@ -4,7 +4,7 @@ cycles = 1024
 mem_dumps = [range(SP - 4, SP)]
 
 def final():
-    assert_reg(r0, 59)
+    assert_reg(r0, 60)
     assert_reg(r1, 0)
     assert_reg(sp_svc, SP)
     assert_mem(SP - 4, 3)
